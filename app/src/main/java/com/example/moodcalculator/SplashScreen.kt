401567ced5a94d05319f6dd9da6d4ffc0ff0df7b
Jermyn-L4-Python <139362743+Jermyn-L4-Python@users.noreply.github.com>
2025-05-29 12:41:22 +0400

    Calc

---
+++ app/src/main/java/com/example/moodcalculator/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.example.moodcalculator
+
+import android.os.Bundle
+import android.os.Looper
+import android.content.Intent
+import android.view.WindowManager
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+
+        }
+    }
